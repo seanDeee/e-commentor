7d0f383288a6411bd19060610ef0fc605af84002
Sean Dee <37123594+seanDeee@users.noreply.github.com>
2018-04-11 01:28:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ define(function (require, exports, module) {
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
         FileUtils = brackets.getModule("file/FileUtils"),
         Dialogs = brackets.getModule("widgets/Dialogs");
-    
+
     // Function to run when the menu item is clicked
-        
+
     function htmlFunction() {
-         var editor = EditorManager.getFocusedEditor();
+        var editor = EditorManager.getFocusedEditor();
         if (editor) {
-            
-   
+
+
             var tmpt =
                 "<!DOCTYPE html>" + "\n" +
                 "<html lang=\"en\">" + "\n" +
@@ -36,15 +36,14 @@ define(function (require, exports, module) {
                 "\n" +
                 "</body>" + "\n" +
                 "\n" +
-                "</html>" + "\n"                  
-            ;
-            
+                "</html>" + "\n";
+
             var insertionPos = editor.getCursorPos();
             editor.document.replaceRange(tmpt, insertionPos);
         }
     }
-    
-    
+
+
     // Menus -> quickLib : display the "About Extension" modal
     function aboutModal() {
         var displayAbout = "<img style=\"float: left; margin:11px 5px 0px 0px; padding:0;\" src=\"styles/images/brackets_icon.svg\" alt=\"logo\" width=\"20\" height=\"20\">";
@@ -56,22 +55,24 @@ define(function (require, exports, module) {
         // show modal dialog with "About Extension" information
         Dialogs.showModalDialog('a', "About Extension", displayAbout);
     }
-    
-  
 
-    
-    var COMMANDO_ID = "htmltemplates.seanDeee";   // package-style naming to avoid collisions
+    var COMMANDO_ID = "htmltemplates.seanDeee"; // package-style naming to avoid collisions
     CommandManager.register("HTML Template", COMMANDO_ID, htmlFunction);
 
-// extension main menu
-    Menus.addMenu('Custom', 'custom.main');
-    var menu = Menus.getMenu('custom.main');
-        //var menu = Menus.getMenu(Menus.AppMenuBar.CUSTOM_MENU);
-    menu.addMenuItem(COMMANDO_ID, [{ "key": "ctrl-1" }, { "key": "ctrl-1"}]);
-    
-    
-      // About Extension menu item
+     // About Extension menu item
     var nav_ABOUT = "about.custom";
     CommandManager.register("About Extension", nav_ABOUT, aboutModal);
+    
+    // extension main menu
+    Menus.addMenu('Custom', 'custom.main');
+    var menu = Menus.getMenu('custom.main');
+    
+    //var menu = Menus.getMenu(Menus.AppMenuBar.CUSTOM_MENU);
+    menu.addMenuItem(COMMANDO_ID, [{
+        "key": "ctrl-1"
+    }, {
+        "key": "ctrl-1"
+    }]);
+
     menu.addMenuItem(nav_ABOUT);
-});                           
+});
